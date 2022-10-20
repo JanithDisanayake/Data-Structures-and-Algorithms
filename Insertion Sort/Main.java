bbd5f60dd67766e193394363f5b11dc9fd807f79
J004 <janith.kavinda3003@gmail.com>
2022-10-20 18:41:46 +0530

    InsertionSort class added to new file

--- Insertion Sort/Main.java
+++ Insertion Sort/Main.java
@@ -1,35 +1,3 @@
-class InsertionSort {
-
-    private int arr[];
-    private int key;
-
-    public InsertionSort(int arr[]) {
-        this.arr = arr;
-    }
-
-    public int[] sort() {
-
-        for(int i = 1; i<arr.length; i++) {
-            key = arr[i];
-            int j = i-1;
-
-            while(j >= 0 && arr[j] > key) {
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = key;
-        }
-        return arr;
-    }
-
-    public void print() {
-        for(int value: this.arr) {
-            System.out.println(value);
-        }
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
 
