bc8db8c39313398ec1e6d40be2d6dbe7ff467500
J004 <janith.kavinda3003@gmail.com>
2022-11-18 20:18:22 +0530

    check the Selection sort working in Main class

--- Selection Sort/Main.java
+++ Selection Sort/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Janith");
+        SelectionSort sc = new SelectionSort();
+        int[] arr = {10,3,6,3,6,7,};
+        arr = sc.sort(arr);
+
+        for(int element : arr) {
+            System.out.println(element);
+        }
     }
 }
